27ef8f4f894dcbe854520cd9553f84d362634684
HermanSher <hermansher1111@gmail.com>
2023-12-09 17:44:02 +0530

    Added map position with Geolocation

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -11,12 +11,19 @@ import {
 import styles from "./Map.module.css";
 import { useEffect, useState } from "react";
 import { useCities } from "../contexts/CitiesContext";
+import { useGeolocation } from "../hooks/useGeoLocation";
+import Button from "./Button";
 
 function Map() {
   const { cities } = useCities();
   const [mapPosition, setMapPosition] = useState([40, 0]);
-
   const [searchParams] = useSearchParams();
+  const {
+    isLoading: isLoadingPosition,
+    position: geoloactionPosition,
+    getPosition,
+  } = useGeolocation();
+
   const mapLat = searchParams.get("lat");
   const mapLng = searchParams.get("lng");
 
@@ -27,8 +34,21 @@ function Map() {
     [mapLat, mapLng]
   );
 
+  useEffect(
+    function () {
+      if (geoloactionPosition)
+        setMapPosition([geoloactionPosition.lat, geoloactionPosition.lng]);
+    },
+    [geoloactionPosition]
+  );
+
   return (
     <div className={styles.mapContainer}>
+      {!geoloactionPosition && (
+        <Button type="position" onClick={getPosition}>
+          {isLoadingPosition ? "Loading..." : "Use your position"}
+        </Button>
+      )}
       <MapContainer
         center={mapPosition}
         zoom={6}
